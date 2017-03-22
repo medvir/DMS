f4bb100222195c9902a02ef8b65976225bcbd45e
ozagordi <osvaldo.zagordi+git@gmail.com>
2017-03-22 14:58:26 +0100

    Checked with shellcheck

--- SampleSheet2openBIS.sh
+++ SampleSheet2openBIS.sh
@@ -8,13 +8,14 @@ bufferdir=/tmp/parse_samples/
 # define these globally so no need to pass it as a function parameter
 headers='undefined'
 rundir='undefined'
+IEMFileVersion='undefined'
 Investigator_Name='undefined'
 Experiment_Name='undefined'
-Date='undefined'
+#Date='undefined'
 Workflow='undefined'
 Application='undefined'
 Assay='undefined'
-Description='undefined'
+#Description='undefined'
 Chemistry='undefined'
 Read1='undefined'
 Read2='undefined'
@@ -45,7 +46,7 @@ write_miseq_run(){
         echo "Directory $dest exists! It should not."
         exit 1
     fi
-    mkdir $dest
+    mkdir "$dest"
     prop_file=${dest}/dataset.properties
 
     datenum=${runname:0:6}
@@ -55,21 +56,22 @@ write_miseq_run(){
 
     ### sample_type MISEQ_RUN
 
-    printf "space=$2\n" > $prop_file
-    printf "IEMFileVersion=$IEMFileVersion\n" >> $prop_file
-    printf "investigator_name=$Investigator_Name\n" >> $prop_file
-    printf "experiment_name=$Experiment_Name\n" >> $prop_file
-    printf "date=${YYYY}-${MM}-${DD}\n" >> $prop_file
-    printf "miseq_workflow=$Workflow\n" >> $prop_file
-    printf "Application=$Application\n" >> $prop_file
-    printf "Assay=$Assay\n" >> $prop_file
-    printf "Chemistry=$Chemistry\n" >> $prop_file
-    printf "Read1=$Read1\n" >> $prop_file
-    printf "Read2=$Read2\n" >> $prop_file
-    printf "PhiX=$PhiX" >> $prop_file
-    printf "RGT_Box_1=$RGT_Box_1\n" >> $prop_file
-    printf "RGT_Box_2=$RGT_Box_2\n" >> $prop_file
-
+    {
+    printf "space=%s\n" "$2"
+    printf "IEMFileVersion=%s\n" "$IEMFileVersion"
+    printf "investigator_name=%s\n" "$Investigator_Name"
+    printf "experiment_name=%s\n" "$Experiment_Name"
+    printf "date=%s-%s-%s\n" "${YYYY}" "${MM}" "${DD}"
+    printf "miseq_workflow=%s\n" "$Workflow"
+    printf "Application=%s\n" "$Application"
+    printf "Assay=%s\n" "$Assay"
+    printf "Chemistry=%s\n" "$Chemistry"
+    printf "Read1=%s\n" "$Read1"
+    printf "Read2=%s\n" "$Read2"
+    printf "PhiX=%s\n" "$PhiX"
+    printf "RGT_Box_1=%s\n" "$RGT_Box_1"
+    printf "RGT_Box_2=%s\n" "$RGT_Box_2"
+    } > "$prop_file"
 }
 
 write_miseq_sample(){
@@ -81,14 +83,16 @@ write_miseq_sample(){
     #echo "    " "${sample_line[@]}"
 
     ### move fastq file into folder
+
     sample_number=${sample_line[0]}
     sample_name=${sample_line[1]}
     sample_plate=${sample_line[2]}
     sample_well=${sample_line[3]}
     I7_index_id=${sample_line[4]}
-    index=${sample_line[5]}
+    index_1=${sample_line[5]}
     I5_index_id=${sample_line[6]}
-    index2=${sample_line[7]}
+    index_2=${sample_line[7]}
+    description=${sample_line[9]}
 
     ### create folder S.. in $dest
     dest=$bufferdir/${rundir}_S${sample_number}
@@ -96,11 +100,11 @@ write_miseq_sample(){
         echo "Directory $dest exists! It should not."
         exit 1
     fi
-    mkdir $dest
+    mkdir "$dest"
 
     fastq_file=$rundir/${sample_name}_S${sample_number}_L001_R1_001.fastq.gz
-    if [ -e $fastq_file ]; then
-        mv $fastq_file ${dest}/
+    if [ -e "$fastq_file" ]; then
+        mv "$fastq_file" "$dest/"
     fi
 
     ### write properties file
@@ -109,7 +113,7 @@ write_miseq_sample(){
     ### space and project
 
     # space is Diagnostics if sample_name is a MOLIS number
-    test_molis $sample_name
+    test_molis "$sample_name"
     if [[ "$ismolis" = true ]];then
         Space='Diagnostics'
     else
@@ -121,35 +125,19 @@ write_miseq_sample(){
     Project=${sample_line[8]}
 
     ### sample_type MISEQ_SAMPLE
-    printf "Space=$Space\n" > $prop_file
-    printf "Project=$Project\n" >> $prop_file
-
-    printf "Sample_ID=${sample_number}\n" >> $prop_file
-
-    Sample_Name_i=$(eval echo "\$Sample_Name_${i}")
-    printf "Sample_Name=${sample_name}\n" >> $prop_file
-
-    Sample_Plate_i=$(eval echo "\$Sample_Plate_${i}")
-    printf "Sample_Plate=${Sample_Plate_i}\n" >> $prop_file
-
-    Sample_Well_i=$(eval echo "\$Sample_Well_${i}")
-    printf "Sample_Well=${Sample_Well_i}\n" >> $prop_file
-
-    I7_Index_ID_i=$(eval echo "\$I7_Index_ID_${i}")
-    printf "I7_Index_ID=${I7_Index_ID_i}\n" >> $prop_file
-
-    index_i=$(eval echo "\$index_${i}")
-    printf "INDEX1=${index_i}\n" >> $prop_file
-
-    I5_Index_ID_i=$(eval echo "\$I5_Index_ID_${i}")
-    printf "I5_Index_ID=${I5_Index_ID_i}\n" >> $prop_file
-
-    index2_i=$(eval echo "\$index2_${i}")
-    printf "INDEX2=${index2_i}\n" >> $prop_file
-
-    Description_i=$(eval echo "\$Description_${i}")
-    printf "DESCRIPTION=${Description_i}\n" >> $prop_file
-
+    {
+    printf "Space=%s\n" "$Space"
+    printf "Project=%s\n" "$Project"
+    printf "Sample_ID=%s\n" "${sample_number}"
+    printf "Sample_Name=%s\n" "${sample_name}"
+    printf "Sample_Plate=%s\n" "${sample_plate}"
+    printf "Sample_Well=%s\n" "$sample_well"
+    printf "I7_Index_ID=%s\n" "$I7_index_id"
+    printf "INDEX_1=%s\n" "$index_1"
+    printf "I5_Index_ID=%s\n" "$I5_index_id"
+    printf "INDEX_2=%s\n" "$index_2"
+    printf "DESCRIPTION=%s\n" "$description"
+    } > "$prop_file"
 }
 
 write_resistance_test(){
@@ -158,8 +146,8 @@ write_resistance_test(){
 
     sample_number=${sample_line[0]}
     sample_name=${sample_line[1]}
-    sample_project=${sample_line[8]}
-    Description=${sample_line[9]}
+    #sample_project=${sample_line[8]}
+    #Description=${sample_line[9]}
     virus=${sample_line[10]}
     genotype=${sample_line[11]}
     target=${sample_line[12]}
@@ -171,17 +159,19 @@ write_resistance_test(){
         echo "Directory $dest exists! It should not."
         exit 1
     fi
-    mkdir $dest
+    mkdir "$dest"
 
     ### write properties file
     prop_file=${dest}/dataset.properties
-    touch $prop_file
+    touch "$prop_file"
 
-    printf "SAMPLE_NAME=$sample_name\n" >> $prop_file
-    printf "VIRUS=$virus\n" >> $prop_file
-    printf "TARGET_TYPE=$target\n" >> $prop_file
-    printf "GENOTYPE=$genotype\n" >> $prop_file
-    printf "VIRAL_LOAD=$viral_load\n" >> $prop_file
+    {
+    printf "SAMPLE_NAME=%s\n" "$sample_name"
+    printf "VIRUS=%s\n" "$virus"
+    printf "TARGET_TYPE=%s\n" "$target"
+    printf "GENOTYPE=%s\n" "$genotype"
+    printf "VIRAL_LOAD=%s\n" "$viral_load"
+    } > "$prop_file"
 
 }
 
@@ -201,7 +191,7 @@ process_runs(){
     headers='undefined'
 
     ### remove spaces in sample sheet
-    sed -e "s/ /_/g" < $incomingdir/$rundir/Data/Intensities/BaseCalls/SampleSheet.csv > sample_sheet.tmp
+    sed -e "s/ /_/g" < "$rundir/Data/Intensities/BaseCalls/SampleSheet.csv" > sample_sheet.tmp
 
     ### counters for read 1/2 and samples
     r=0
@@ -209,10 +199,10 @@ process_runs(){
     diag_sample=false
     res_sample=false
     ### read sample sheet line by line
-    while IFS=',' read -a line
+    while IFS=',' read -r -a line
     do
 
-        if [[ ${line[0]} == \[*] ]]
+        if [[ ${line[0]} =~ ^\[[[:alpha:]]*\] ]]
         then
             section=${line[0]}
 
@@ -244,7 +234,7 @@ process_runs(){
         elif [[ $section == "[Data]" && ${line[1]} && $s -eq 0 ]]
         then
             headers=( "${line[@]}" )
-            echo -e '  Headers found:' ${headers[@]}
+            echo -e "  Headers found:" "${headers[@]}"
             ((s+=1))
 
         ### [Data] section values
@@ -252,7 +242,7 @@ process_runs(){
         then
             # echo "Before: " "$ismolis"
             echo "    " "${line[@]}"
-            test_molis ${line[1]}
+            test_molis "${line[1]}"
             if [ "$ismolis" = true ]; then
                 diag_sample=true
             else
@@ -274,22 +264,22 @@ process_runs(){
     # if any sample was "diagnostics" then write diagnostic run
     if [ "$diag_sample" = true ]; then
         echo "WRITING DIAGNOSTICS"
-        write_miseq_run ${rundir}_DIA Diagnostics
+        write_miseq_run "${rundir}_DIA" Diagnostics
     fi
     # if any sample was "research" then write research run
     if [ "$res_sample" = true ]; then
         echo "WRITING RESEARCH"
-        write_miseq_run ${rundir}_RES Research
+        write_miseq_run "${rundir}_RES" Research
     fi
 
     # reset [Header] and [Reads] information
     Investigator_Name='undefined'
     Experiment_Name='undefined'
-    Date='undefined'
+#    Date='undefined'
     Workflow='undefined'
     Application='undefined'
     Assay='undefined'
-    Description='undefined'
+#    Description='undefined'
     Chemistry='undefined'
     Read1='undefined'
     Read2='undefined'
@@ -298,18 +288,18 @@ process_runs(){
     RGT_Box_2='undefined'
     openbis='n'
 
-    touch $rundir/.UPLOADED_RUN
+    touch "$rundir/.UPLOADED_RUN"
 
 }
 
 echo 'GO!'
 ### main loop over all dirs in $incomingdir starting with "1"
-#for run in $(find $incomingdir -type d -name "1*" -depth 1); do
-for rundir in $(ls $incomingdir); do
-    if [[ -d $incomingdir/$rundir ]]
-    then
-        echo -e "\033[1;31m================================================================================\033[0m"
-        echo 'Now syncing:' $rundir
-        process_runs $rundir
-    fi
+#for rundir in $(find $incomingdir -type d -name "1*" -depth 1)
+for rundir in $(ls $incomingdir)
+#for rundir in "$incomingdir"/1*
+do
+    [[ -d "$rundir" ]] || continue
+    echo -e "\033[1;31m================================================================================\033[0m"
+    echo "Now syncing:" "$rundir"
+    process_runs "$rundir"
 done
