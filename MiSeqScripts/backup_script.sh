7db12b330a8a66f84ccb4862de6baf8c9f6d0e22
ozagordi <osvaldo.zagordi+git@gmail.com>
2018-03-07 10:59:42 +0100

    Fix a strange line cut

--- MiSeqScripts/backup_script.sh
+++ MiSeqScripts/backup_script.sh
@@ -5,8 +5,7 @@ DST=/backup/MiSeq
 echo '' >> backup.log
 echo '--------------------' >> backup.log
 date >> backup.log
-rsync -av --stats --exclude='Images*' --exclude='Alignment*' $SRC virologysrv10
-.uzh.ch:$DST &>> backup.log
+rsync -av --stats --exclude='Images*' --exclude='Alignment*' $SRC virologysrv10.uzh.ch:$DST &>> backup.log
 echo 'backed up' >> backup.log
 date >> backup.log
 echo '--------------------' >> backup.log
