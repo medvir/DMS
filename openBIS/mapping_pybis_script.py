771247b424d1e7df72aa4ec7fbe7e758e866e7c9
Michael Huber <huber.michael@virology.uzh.ch>
2018-12-06 13:53:14 +0100

    Increased waiting time before analysis session starting to 60 s

--- openBIS/mapping_pybis_script.py
+++ openBIS/mapping_pybis_script.py
@@ -197,7 +197,7 @@ for pro in ['antibodies', 'resistance', 'metagenomics', 'plasmids', 'other']:
     general_mapping(pro)
 logging.info('-----------Mapping session finished------------')
 logging.info('* * * * * * * * * * * * * * * * * * * * * * * *')
-time.sleep(30)
+time.sleep(60)
 logging.info('-----------Analysis session starting-----------')
 
 # Fetch all resistance samples that are mapped
