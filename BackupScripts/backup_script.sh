6446618279fa1e543ae8ce59931542a1efc3328f
sschmutz <schmutz.stefan@virology.uzh.ch>
2019-06-28 10:07:59 +0200

    change backup.log location to DMS repo

--- BackupScripts/backup_script.sh
+++ BackupScripts/backup_script.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 SRC=/cygdrive/D/Illumina/MiSeqOutput/
 DST=/backup/MiSeq
+LOG=/cygdrive/c/Users/sbsuser/DMS/BackupScripts/backup.log
 
-echo '' >> backup.log
-echo '--------------------' >> backup.log
-date >> backup.log
-rsync -av --stats --exclude='Images*' --exclude='Alignment*' $SRC virologysrv10.uzh.ch:$DST &>> backup.log
-echo 'backed up' >> backup.log
-date >> backup.log
-echo '--------------------' >> backup.log
-echo '' >> backup.log
+echo '' >> $LOG
+echo '--------------------' >> $LOG
+date >> $LOG
+rsync -av --stats --exclude='Images*' --exclude='Alignment*' $SRC virologysrv10.uzh.ch:$DST &>> $LOG
+echo 'backed up' >> $LOG
+date >> $LOG
+echo '--------------------' >> $LOG
+echo '' >> $LOG
