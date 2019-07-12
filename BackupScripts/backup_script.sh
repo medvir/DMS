a3206d912043d38cf80b5909f7eb725d0418a70d
sschmutz <stefan.schmutz@outlook.com>
2019-07-12 15:20:54 +0200

    write standarderror from weekly backup separate

--- BackupScripts/backup_script.sh
+++ BackupScripts/backup_script.sh
@@ -2,12 +2,13 @@
 SRC=/cygdrive/D/Illumina/MiSeqOutput/
 DST=/backup/MiSeq
 LOG=/cygdrive/c/Users/sbsuser/DMS/BackupScripts/backup.log
+ERR=/cygdrive/c/Users/sbsuser/DMS/BackupScripts/backup.err
 
-echo '' >> $LOG
-echo '--------------------' >> $LOG
-date >> $LOG
-rsync -av --stats --exclude='Images*' --exclude='Alignment*' $SRC virologysrv10.uzh.ch:$DST &>> $LOG
-echo 'backed up' >> $LOG
-date >> $LOG
-echo '--------------------' >> $LOG
-echo '' >> $LOG
+echo '' >> $LOG 2>> $ERR
+echo '--------------------' >> $LOG 2>> $ERR
+date >> $LOG 2>> $ERR
+rsync -av --stats --exclude='Images*' --exclude='Alignment*' $SRC virologysrv10.uzh.ch:$DST &>> $LOG 2>> $ERR
+echo 'backed up' >> $LOG 2>> $ERR
+date >> $LOG 2>> $ERR
+echo '--------------------' >> $LOG 2>> $ERR
+echo '' >> $LOG 2>> $ERR
