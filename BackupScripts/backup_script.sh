27c30402a64852c5bf781ae23a572cbaf64d6df2
sschmutz <stefan.schmutz@outlook.com>
2021-09-10 08:08:38 +0200

    stop excluding the Alignment folder from backing up as the fastq files are now in there

--- BackupScripts/backup_script.sh
+++ BackupScripts/backup_script.sh
@@ -7,7 +7,7 @@ ERR=/cygdrive/c/Users/sbsuser/DMS/BackupScripts/backup.err
 echo '' >> $LOG 2>> $ERR
 echo '--------------------' >> $LOG 2>> $ERR
 date >> $LOG 2>> $ERR
-rsync -av --stats --exclude='Images*' --exclude='Alignment*' $SRC virologysrv10.uzh.ch:$DST &>> $LOG 2>> $ERR
+rsync -av --stats --exclude='Images*' $SRC virologysrv10.uzh.ch:$DST &>> $LOG 2>> $ERR
 echo 'backed up' >> $LOG 2>> $ERR
 date >> $LOG 2>> $ERR
 echo '--------------------' >> $LOG 2>> $ERR
